6c665440699ed6baa4732ba862c1d30ae04ad0ad
childhappy1101 <childhappy@126.com>
2022-05-01 17:54:06 +0800

    delete

--- src/main/java/com/rays/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.rays.mapper;
-
-import com.rays.bean.UserBean;
-
-public interface UserMapper {
-    UserBean getUser(int userId);
-}
--- src/main/java/com/rays/service/IUserService.java
+++
@@ -1,7 +0,0 @@
-package com.rays.service;
-
-import com.rays.bean.UserBean;
-
-public interface IUserService {
-    public UserBean getUser(int userId);
-}
